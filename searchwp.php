f4e045f41c918d168ea1fae478389fdbd1c241c5
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-02-28 15:14:15 +0530

    Update searchwp.php

--- searchwp.php
+++ searchwp.php
@@ -1,5 +1,43 @@
 
 /**
+ * Add short code for the widget
+ */
+function func_custom_search($atts) {
+    
+    global $wp_widget_factory;
+    
+    extract(shortcode_atts(array(
+        'widget_name' => 'custom_search_widget'
+    ), $atts));
+    
+    $widget_name = wp_specialchars($widget_name);
+    
+    if (!is_a($wp_widget_factory->widgets[$widget_name], 'WP_Widget')):
+        $wp_class = 'WP_Widget_'.ucwords(strtolower($class));
+        
+        if (!is_a($wp_widget_factory->widgets[$wp_class], 'WP_Widget')):
+            return '<p>'.sprintf(__("%s: Widget class not found. Make sure this widget exists and the class name is correct"),'<strong>'.$class.'</strong>').'</p>';
+        else:
+            $class = $wp_class;
+        endif;
+    endif;
+    
+    ob_start();
+    the_widget($widget_name, $instance, array('widget_id'=>'arbitrary-instance-'.$id,
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '',
+        'after_title' => ''
+    ));
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
+    
+}
+add_shortcode('custom_search','func_custom_search'); 
+
+
+/**
  * Adding a custom search widget
  */
 class Custom_Search_Widget extends WP_Widget{
