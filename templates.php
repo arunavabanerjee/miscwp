359414326b4ad678a5f07b64a0cbe67d8511df30
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-03-03 13:35:56 +0530

    Update templates.php

--- templates.php
+++ templates.php
@@ -1,4 +1,106 @@
  
+//---wordpress template
+<!-- Container -->
+<div class="container posts-conainer-loop <?php echo $layout; ?>">
+
+<div class="twelve columns">
+    <div class="extra-padding post-row">
+
+    <?php if ( have_posts() ) : ?>
+
+     <?php //get values of locations
+           $field_key = "location"; $locations = array();
+           $field = get_field_object($field_key);
+           if( $field ){ foreach( $field['choices'] as $k => $v ){ array_push($locations, $v); }} ?>
+
+     <?php foreach($locations as $loc){ ?>
+
+      <?php
+        $metaqry = array(); $metaqry["relation"] = "AND";  
+        $intrim = array( 'key' => "location", 'value' => $loc, 'compare' => '==' ); array_push($metaqry, $intrim);
+        $args = array('numberposts' => -1,'post_type' => 'post', 'cat' => 72, 'meta_query' => $metaqry); 
+        query_posts($args); //echo $GLOBALS['wp_query']->request;
+      ?>
+      <?php global $wp_query; 
+       if( $wp_query->found_posts > 0 ) { ?>
+
+      <div class=""> 
+       <p>Location: <?php echo $loc; ?> </p>
+
+       <?php while ( have_posts() ) : the_post(); ?> 
+           <?php
+                /* Include the Post-Format-specific template for the content.
+                 * If you want to override this in a child theme, then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                $format = get_post_format();
+                if( false === $format  )  $format = 'standard';
+                get_template_part( 'postformats/content', $format );
+            ?>
+        <?php endwhile; ?>
+
+      </div>
+      <div class="clearfix"></div>
+      
+      <?php wp_reset_query(); ?>
+
+      <?php } // endif ?>
+
+     <?php } // endforeach ?>
+
+    <?php else : ?>
+        <?php get_template_part( 'content', 'none' ); ?>
+    <?php endif; ?>
+
+        <div class="clearfix"></div>
+
+        <!-- Pagination -->
+        <div class="pagination-container">
+            <?php if(function_exists('wp_pagenavi')) { ?>
+            <nav class="pagination">
+                 <?php wp_pagenavi(); ?>
+            </nav>
+            <?php
+            } else {
+            if ( get_next_posts_link() ||  get_previous_posts_link() ) : ?>
+            <nav class="pagination">
+                <ul>
+                    <?php if ( get_previous_posts_link() ) : ?>
+                        <li id="next"><?php previous_posts_link( ' ' ); ?></li>
+                    <?php  endif; ?>
+                    <?php if ( get_next_posts_link() ) : ?>
+                        <li id="prev"><?php next_posts_link( ' ' ); ?></li>
+                        <!-- <li><a href="#" class="next"></a></li> -->
+                     <?php endif; ?>
+                </ul>
+            </nav>
+           <?php endif;
+           } ?>
+        </div>
+
+    </div>
+</div>
+
+<?php get_sidebar(); ?>
+
+//----------------------------------------------------------------------------------------------------
+
+
+// ACF field list
+$field_key = "field_5039a99716d1d";
+$field = get_field_object($field_key);
+if( $field )
+{
+    echo '<select name="' . $field['key'] . '">';
+        foreach( $field['choices'] as $k => $v )
+        {
+            echo '<option value="' . $k . '">' . $v . '</option>';
+        }
+    echo '</select>';
+}
+
+//------------------------------------------------------------------------
+
 <script>
 jQuery(document).ready(function(){ 
 
