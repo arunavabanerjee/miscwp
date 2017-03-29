c12646afeefa9a4c88a17f4fe3e3a1c2693971ed
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-03-29 14:31:29 +0530

    Update templates.php

--- templates.php
+++ templates.php
@@ -292,6 +292,25 @@ if($orderPerHr < $maxOrderPerHr){
    $inc = 0; update_field('orders_per_hour', $inc , $restaurant->ID); 
 }
 
+//-------------------------------------------------------------------------------------------
+	
+/*
+ * get all custom fields, loop through them and load the field object 
+ * to create a label => value markup
+ */
+$fields = get_fields(get_the_ID()); var_dump($fields);
+//if( $fields ){  
+//  foreach( $fields as $field_name => $value ){
+//     // get_field_object( $field_name, $post_id, $options )
+//     // - $value has already been loaded for us, no point to load it again in the get_field_object function
+//     $field = get_field_object($field_name, false, array('load_value' => false));
+//     echo '<div>'; echo '<h3>' . $field['label'] . '</h3>'; echo $value; echo '</div>';
+//  }
+//}
+	
+	
+	
+	
 	
 	
 	
