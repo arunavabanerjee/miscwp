6b5407abcd010f56a3f57593fc49fcf86ca69e03
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-05-03 11:22:19 +0530

    Update misctemplate-tags.php

--- misctemplate-tags.php
+++ misctemplate-tags.php
@@ -1,4 +1,15 @@
 
+/**
+ * redirect user to a custom login page on login
+ */
+function quickfood_login_redirect( $redirect_to, $request, $user ){
+  //var_dump($user);
+  $login_dashboard = site_url().'/'.'dashboard.php';
+  return ( is_array( $user->roles ) && in_array( 'customer', $user->roles ) ) ? $login_dashboard : admin_url();
+}
+add_filter( 'login_redirect', 'quickfood_login_redirect', 10, 3 );
+
+
 //--- breadcrumbs
 function get_post_breadcrumbs(){
  $bcrumbs = array(); $html = ''; 
