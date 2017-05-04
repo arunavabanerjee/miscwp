3cec85ef4d3a662370501dfead8af67bd56c692c
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-05-04 12:01:42 +0530

    Create emailtemplate.php

---
+++ emailtemplate.php
@@ -0,0 +1,168 @@
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+        <td bgcolor="#009cff">
+            <!-- HIDDEN PREHEADER TEXT -->
+            <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
+                Entice the open with some amazing preheader text. Use a little mystery and get those subscribers to read through... 
+            </div>
+            <div align="center" style="padding: 0px 15px 0px 15px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="600" class="wrapper" style="border:10px solid #0071B9;">
+                    <tbody style="background:#ffffff;">
+                        <tr style="border-bottom: 5px solid #0071B9;">
+                            <td style="padding: 20px 0px 30px 0px;" class="logo" bgcolor="#0071B9">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="150" align="left" style="padding-left:20px;">
+                                            <a href="#" target="_blank">
+                                                <img alt="Logo" src="http://lab-4.sketchdemos.com/P-491-SISCO/wp-content/uploads/2016/01/logo.png" width="100%" height="auto" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 16px;" border="0">
+                                            </a>
+                                        </td>
+                                        <td width="450" align="right" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #ffffff; text-decoration: none;"><h2 class="entry-title" style="text-transform:uppercase; padding-right:20px;">Service &amp; Support Welding</h2></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        
+                        <tr style="border-bottom: 5px solid #0071B9;">
+                            <td style="padding: 20px 0px 30px 0px;" class="logo">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="200" align="right">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><b class="entry-title" style="text-transform:uppercase; padding-right:20px;">Model</b></td>
+                                                </tr>
+                                            </table>                                            
+                                        </td>
+                                        <td width="400" align="left" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="left" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><span class="entry-title" style="text-transform:uppercase; padding-right:20px;">[model]</span></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="200" align="right">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><b class="entry-title" style="text-transform:uppercase; padding-right:20px;">Company</b></td>
+                                                </tr>
+                                            </table>                                            
+                                        </td>
+                                        <td width="400" align="left" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="left" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><span class="entry-title" style="text-transform:uppercase; padding-right:20px;">[Company]</span></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="200" align="right">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><b class="entry-title" style="text-transform:uppercase; padding-right:20px;">Address</b></td>
+                                                </tr>
+                                            </table>                                            
+                                        </td>
+                                        <td width="400" align="left" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="left" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><span class="entry-title" style="text-transform:uppercase; padding-right:20px;">[Address]</span></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="200" align="right">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><b class="entry-title" style="text-transform:uppercase; padding-right:20px;">Email</b></td>
+                                                </tr>
+                                            </table>                                            
+                                        </td>
+                                        <td width="400" align="left" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="left" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><span class="entry-title" style="text-transform:uppercase; padding-right:20px;">[email]</span></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="200" align="right">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><b class="entry-title" style="text-transform:uppercase; padding-right:20px;">Contact Person</b></td>
+                                                </tr>
+                                            </table>                                            
+                                        </td>
+                                        <td width="400" align="left" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="left" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><span class="entry-title" style="text-transform:uppercase; padding-right:20px;">[contactperson]</span></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="200" align="right">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><b class="entry-title" style="text-transform:uppercase; padding-right:20px;">Phone Number</b></td>
+                                                </tr>
+                                            </table>                                            
+                                        </td>
+                                        <td width="400" align="left" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="left" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><span class="entry-title" style="text-transform:uppercase; padding-right:20px;">[phone]</span></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr style="border-bottom: 5px solid #0071B9;">
+                                        <td width="200" align="right" style="vertical-align: top; padding-top: 20px; white-space: nowrap;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="right" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none;"><b class="entry-title" style="text-transform:uppercase; padding-right:20px;">Complaint Details</b></td>
+                                                </tr>
+                                            </table>                                            
+                                        </td>
+                                        <td width="400" align="left" class="text-td" style="vertical-align: middle; display: table-cell;">
+                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                <tr>
+                                                    <td align="justify" style="padding: 0 0 5px 0; font-size: 14px; font-family: Arial, sans-serif; color: #666666; text-decoration: none; padding:20px; padding-left:0;"><span class="entry-title" style="text-transform:uppercase; padding-right:20px;">[details]</span></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+        <td bgcolor="#dddddd" align="center" style="padding: 20px 0px;">
+            <!-- UNSUBSCRIBE COPY -->
+            <table width="500" border="0" cellspacing="0" cellpadding="0" align="center" class="responsive-table">
+                <tbody><tr>
+                    <td align="center" style="font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
+                        <span class="appleFooter" style="color:#666666;">15, Convent Road Kolkata - 700014</span>
+                    </td>
+                </tr>
+            </tbody></table>
+        </td>
+    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </td>
+    </tr>
+</table>
