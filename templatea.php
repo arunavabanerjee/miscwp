75ae712656a2d40ba02fa8ea9d127ee97e943ff8
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-05-09 13:23:32 +0530

    Update templatea.php

--- templatea.php
+++ templatea.php
@@ -1,4 +1,18 @@
 <?php 
+// Function to change email address
+function wpb_sender_email( $original_email_address ) {
+    return 'tim.smith@example.com';
+}
+// Function to change sender name
+function wpb_sender_name( $original_email_from ) {
+    return 'Tim Smith';
+}
+// Hooking up our functions to WordPress filters
+add_filter( 'wp_mail_from', 'wpb_sender_email' );
+add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
+
+///---------------------------------------------------------
+
 //change email address and sender name
 add_filter( 'wp_mail_from', 'your_email' );
 function your_email( $original_email_address ) { return get_option('admin_email'); } 
