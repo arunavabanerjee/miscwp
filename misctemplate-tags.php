8aa2141f6563514ed03dd3c4037556b0032e7c56
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-05-12 15:02:34 +0530

    Update misctemplate-tags.php

--- misctemplate-tags.php
+++ misctemplate-tags.php
@@ -1,4 +1,27 @@
 
+========================
+if( null == username_exists( $email_address ) ) {
+  // Generate the password and create the user
+  $password = wp_generate_password( 12, false );
+  $user_id = wp_create_user( $email_address, $password, $email_address );
+ 
+  // Set the nickname
+  wp_update_user(
+    array(
+      'ID'          =>    $user_id,
+      'nickname'    =>    $email_address
+    )
+  );
+ 
+  // Set the role
+  $user = new WP_User( $user_id );
+  $user->set_role( 'contributor' );
+ 
+  // Email the user
+  wp_mail( $email_address, 'Welcome!', 'Your Password: ' . $password );
+} // end if 
+
+
 /**
  * redirect user to a custom login page on login
  */
