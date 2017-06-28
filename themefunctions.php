27d75525ad22af9f50ca4a7cae6049da5e7840b0
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-06-28 11:13:41 +0000

    Update themefunctions.php

--- themefunctions.php
+++ themefunctions.php
@@ -1,5 +1,174 @@
 
-///----script
+///------- script
+/**
+ * Theme functions file.
+ *
+ * Contains handlers for navigation and widget area.
+ */
+
+$(document).ready(function(){
+ $('.calendar-section #select-region').trigger('change');
+});
+
+//view button click on home page
+$('.page-template-frontpage-tmp .calendar-section .calendar-table .view-btn').click(function(){ 
+  var getSelRegion = $('.page-template-frontpage-tmp .calendar-section #select-region').val(); //alert(getSelRegion);
+  var siteurl = window.location.href; 
+  window.location.href = siteurl + "games/?region=" + getSelRegion;
+});
+
+//games page on load
+$('body .page-template-games-tmpl').ready(function(){ 
+ //if the url contains a parameter 
+ if( window.location.href.indexOf("?") == -1 ){  
+   $('.game-section .select-region').trigger('change'); 
+ }
+ if( window.location.href.indexOf("?") > -1 ){
+   var searchReg = window.location.search.substr(1);
+   var region = searchReg.split('='); alert(region[1]);
+   $('.game-section .select-region').val(region[1]).change();
+   $('.game-section .select-region').trigger('change'); 
+ }
+});
+
+//changes to the table on games page
+$('.game-section .calendar .calendar-day-np').click(function(){ 
+  var $chkbox = $(this).find('input[type="checkbox"]');
+  var status = $chkbox.prop('checked'); 
+  var val = $chkbox.prop('value'); //alert(status); alert(val); 
+  $('.game-section .select-region').trigger('change');
+});
+
+
+//------------
+// home page
+//------------
+//uses the change region to revamp games table on home page
+$('.calendar-section #select-region').change(function(){
+   var getRegionID = $('.calendar-section #select-region').val(); //alert(getRegionID);
+   var getRegionText = $('.calendar-section #select-region option:selected').text(); 
+
+   // input region code to revamp the table
+   $.ajax({  
+      url: frontendajaxurl.ajaxurl,  
+      method : "POST",  
+      dataType: "html",
+      data:{ 
+	'action':'action_newrevamptable',
+        'regionID':getRegionID,
+      },
+      success:function(response){
+          console.log('success'); 
+	  var tablehtml = getRegionText + ' Games';
+	  $('.calendar-section .changetable').html(tablehtml);
+	  $(".calendar-table").find(".padding0").html( response );
+      }
+   }); 
+
+   //ajax_url ='http://lab-1.sketchdemos.com/P943_Basketball/wp-admin/admin-ajax.php';
+   /*ajax_url = '<?php echo admin_url('admin-ajax.php'); ?>';
+   $.post(ajax_url, {
+        action: 'action_revamptable',  
+        type:"POST",
+        regionID:getRegionID, 
+        }, 
+	function (response) { 
+          $(".calendar-table").find(".padding0").html( response );    
+          //console.log(response); //return false;
+          //$('.right-ber').html(response);                    
+   	}
+    );*/
+});
+
+//checks the click on the anchor of the table
+$(document).on("click", '.calendar-table .padding0 .sub-table a', function(event) { 
+
+ var id = $(this).attr('data-id');
+
+ //enter id to revamp the modal
+ $.ajax({  
+   url: frontendajaxurl.ajaxurl,  
+   method : "POST",  
+   dataType: "html",
+   data:{ 
+	'action':'action_modaldata',
+        'id':id,
+   },
+   success:function(response){
+     console.log('modal success'); 
+     $('.display_modal').html(response); 
+     $('.display_modal #exampleModalLong').modal('show');
+   }
+ }); 
+});
+
+//--------------
+//games page
+//--------------
+//uses the change region to revamp games table on home page
+$('.game-section .select-region').change(function(){
+   var getRegionID = $('.game-section .select-region').val(); //alert(getRegionID);
+   var getRegionText = $('.game-section .select-region option:selected').text(); 
+   var checkboxes = $('input[type="checkbox"][name="dates"]:checked'); var dates = '';
+   for (i=0; i < checkboxes.length; i++){ var chkbx = checkboxes.get(i); dates += chkbx.value +','; } 
+   //alert(dates);
+
+   // input region code to revamp the table
+   $.ajax({  
+      url: frontendajaxurl.ajaxurl,  
+      method : "POST",  
+      dataType: "html",
+      data:{ 
+	'action':'action_gamesrevamptable',
+        'regionID':getRegionID,
+	'dates':dates,
+      },
+      success:function(response){
+          console.log('success'); 
+	  var tablehtml = getRegionText + ' Games';
+	  $('.game-section .changetable').html(tablehtml);
+	  $(".game-section").find(".changegametable").html( response );
+      }
+   }); 
+
+   //ajax_url ='http://lab-1.sketchdemos.com/P943_Basketball/wp-admin/admin-ajax.php';
+   /*ajax_url = '<?php echo admin_url('admin-ajax.php'); ?>';
+   $.post(ajax_url, {
+        action: 'action_revamptable',  
+        type:"POST",
+        regionID:getRegionID, 
+        }, 
+	function (response) { 
+          $(".calendar-table").find(".padding0").html( response );    
+          //console.log(response); //return false;
+          //$('.right-ber').html(response);                    
+   	}
+    );*/
+});
+
+//checks the click on the anchor of the table
+$(document).on("click", '.game-table .table a', function(event) { 
+
+ var id = $(this).attr('data-id');
+
+ //enter id to revamp the modal
+ $.ajax({  
+   url: frontendajaxurl.ajaxurl,  
+   method : "POST",  
+   dataType: "html",
+   data:{ 
+	'action':'action_modaldata',
+        'id':id,
+   },
+   success:function(response){
+     console.log('modal success'); 
+     $('.games_display_modal').html(response); 
+     $('.games_display_modal #exampleModalLong').modal('show');
+   }
+ }); 
+});
+
+///------------------------------------------------------------script
 /* global screenReaderText */
 /**
  * Theme functions file.
