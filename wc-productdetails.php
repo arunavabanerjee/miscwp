c8d3d682bebe5fe1a65934de202b492abdb24a98
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-11-27 20:10:52 +0530

    Update wc-productdetails.php

--- wc-productdetails.php
+++ wc-productdetails.php
@@ -1,4 +1,22 @@
 
+/**
+ * Generate custom search form
+ *
+ * @param string $form Form HTML.
+ * @return string Modified form HTML.
+function wpdocs_my_search_form( $form ) {
+    $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
+    <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
+    <input type="text" value="' . get_search_query() . '" name="s" id="s" />
+    <input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
+    </div>
+    </form>';
+ 
+    return $form;
+}
+add_filter( 'get_search_form', 'wpdocs_my_search_form' );
+
+----------------------------------------------
 First, copy the  woocommerce.php file to your child theme and replace the content with the following code:
 
 <?php
