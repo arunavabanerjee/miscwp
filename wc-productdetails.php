a0412a8efa9bba06acfbf6752452de100611891d
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-11-27 18:20:54 +0530

    Update wc-productdetails.php

--- wc-productdetails.php
+++ wc-productdetails.php
@@ -1,4 +1,76 @@
 
+First, copy the  woocommerce.php file to your child theme and replace the content with the following code:
+
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package zerif
+ */
+get_header(); ?>
+<div class="clear"></div>
+</header> <!-- / END HOME SECTION  -->
+<div id="content" class="site-content">
+	<div class="container">
+		<div class="content-left-wrap col-md-9">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
+					<?php woocommerce_content(); ?>
+				</main><!-- #main -->
+			</div><!-- #primary -->
+		</div><!-- .content-left-wrap -->
+		<div class="sidebar-wrap col-md-3 content-left-wrap">
+			<?php get_sidebar(); ?>
+		</div>
+	</div><!-- .container -->
+<?php get_footer(); ?>
+Now copy also the  page.php file to your child theme, and replace its content with this:
+
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package zerif
+ */
+get_header(); ?>
+<div class="clear"></div>
+</header> <!-- / END HOME SECTION  -->
+<div id="content" class="site-content">
+	<div class="container">
+		<div class="content-left-wrap col-md-9">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
+					<?php 
+					while ( have_posts() ) : 
+						the_post();
+						get_template_part( 'content', 'page' );
+						/* If comments are open or we have at least one comment, load up the comment template */
+						if ( comments_open() || '0' != get_comments_number() ) :
+							comments_template();
+						endif;
+					endwhile;
+				?>
+				</main><!-- #main -->
+			</div><!-- #primary -->
+		</div>
+		<div class="sidebar-wrap col-md-3 content-left-wrap">
+			<?php get_sidebar(); ?>
+		</div>
+	</div><!-- .container -->
+<?php get_footer(); ?>
+
+------------------------------------------------------------------
+
 WooCommerce Single Product Page Default add_actions
 
 // These are actions you can unhook/remove!
