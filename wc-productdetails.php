c0a2d180600a020bb7c310f6fa867fcf81a8e716
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-11-29 16:06:52 +0530

    Update wc-productdetails.php

--- wc-productdetails.php
+++ wc-productdetails.php
@@ -1,4 +1,22 @@
 
+
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' );    // 2.1 +
+ 
+function woo_custom_cart_button_text() {
+ 
+        return __( 'My Button Text', 'woocommerce' );
+ 
+}
+
+add_filter( 'woocommerce_product_add_to_cart_text', 'woo_archive_custom_cart_button_text' );    // 2.1 +
+ 
+function woo_archive_custom_cart_button_text() {
+ 
+        return __( 'My Button Text', 'woocommerce' );
+ 
+}
+
+------------------------------------------------------------------
 /**
  * Generate custom search form
  *
